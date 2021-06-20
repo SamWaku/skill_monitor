153cd66252445f1d91b9432f7e9e8e68cf992f26
SamWaku <56162251+SamWaku@users.noreply.github.com>
2021-06-20 22:35:12 +0200

    get, add, delete, items_loading functions

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { GET_ITEMS, DELETE_ITEM, ADD_ITEM, ITEMS_LOADING } from './types';
+
+export const getItems = () => dispatch => {
+    dispatch(setItemsLoading);
+    axios
+        .get('/api/items')
+        .then(res => 
+            dispatch({
+                type: GET_ITEMS,
+                payload: res.data
+            })
+        )
+};
+
+export const addItem = item => {
+    return {
+        type: ADD_ITEM,
+        payload: item
+    };
+};
+
+export const deleteItem = id => {
+    return {
+        type: DELETE_ITEM, 
+        payload: id
+    };
+};
+
+
+export const setItemsLoading = () => {
+    return {
+        type: ITEMS_LOADING
+    }
+}
