409a7a845bb9f35116f557cd57f121ac8247abd0
SamWaku <56162251+SamWaku@users.noreply.github.com>
2021-06-24 13:14:21 +0200

    loading items from database and adding item

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -2,22 +2,24 @@ import axios from 'axios';
 import { GET_ITEMS, DELETE_ITEM, ADD_ITEM, ITEMS_LOADING } from './types';
 
 export const getItems = () => dispatch => {
-    dispatch(setItemsLoading);
-    axios
-        .get('/api/items')
-        .then(res => 
+    dispatch(setItemsLoading());
+    axios.get('/api/items').then(res => 
             dispatch({
                 type: GET_ITEMS,
                 payload: res.data
             })
-        )
+        );
 };
 
-export const addItem = item => {
-    return {
-        type: ADD_ITEM,
-        payload: item
-    };
+export const addItem = item => dispatch => {
+    axios
+    .post('/api/items', item)
+    .then(res =>
+         dispatch({
+             type: ADD_ITEM,
+             payload: res.data
+         })
+        )
 };
 
 export const deleteItem = id => {
